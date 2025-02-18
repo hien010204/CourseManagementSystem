6a1628a412827b10dfbfd133793416eff47f4b28
BuiXuanHien <155499678+qe180008@users.noreply.github.com>
2025-02-18 20:52:50 +0700

    list student in course and status

---
+++ CourseManagementSystem/DTO/StudentCourseInfoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseManagementSystem.DTO
+{
+    public class StudentCourseInfoDto
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
--- CourseManagementSystem/Services/Courses/ICourseService.cs
+++ CourseManagementSystem/Services/Courses/ICourseService.cs
@@ -15,5 +15,7 @@ namespace CourseManagementSystem.Services.Models
         bool EnrollInCourse(int courseId, int userId);
         List<CourseDto> GetUserCourses(int userId);
         public bool ConfirmEnrollment(int courseId, int studentId);
+        public List<StudentCourseInfoDto> GetStudentsAndCourses(string enrollmentStatus);
+        public List<StudentCourseInfoDto> GetConfirmedStudentsInCourse(int courseId);
     }
 }
